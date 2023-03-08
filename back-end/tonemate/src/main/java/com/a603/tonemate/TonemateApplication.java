dc6cb1afda62606bd481c63d3c930ad83a1315c9
이상현 <dkrdj1000@naver.com>
2023-03-08 16:50:35 +0900

    스프링 프로젝트 생성 및 하둡 폴더 생성

---
+++ back-end/tonemate/src/main/java/com/a603/tonemate/TonemateApplication.java
@@ -0,0 +1,13 @@
+package com.a603.tonemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonemateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TonemateApplication.class, args);
+    }
+
+}
