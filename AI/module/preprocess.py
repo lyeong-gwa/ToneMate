5f67dd643ec48c15b04dc30dba35029b065859f3
RyuJeongmin <jmyoo88@naver.com>
2023-03-15 13:50:38 +0900

    [AI : Feat - 음원 데이터에서 추출하는 특성 추가]

--- AI/module/preprocess.py
+++ AI/module/preprocess.py
@@ -3,37 +3,88 @@ import librosa
 import numpy as np
 from keras.utils import to_categorical
 
+# 데이터 크기 조정
+def resize(feature, new_size):
+    # 1차원인 경우
+    if len(feature)==1:        
+        pad_width = (0, new_size - len(feature[0]))
+    # 다차원인 경우    
+    elif len(feature) > 1:
+        pad_width = [(0, 0), (0, new_size - len(feature[0]))]
+    
+    if len(feature[0]) < new_size:
+        feature = np.pad(feature, pad_width, mode='constant', constant_values=0)
+    else:
+        feature = feature[:, :new_size]
+    return feature
+
 # wav 파일 불러오기
 def load_wav_file(file_path, sr=16000):
     new_size = 3000
     y, _ = librosa.load(file_path, sr=sr)
-    mfcc_features = librosa.feature.mfcc( y = y, sr = sr)
-    if len(mfcc_features[1]) < new_size:
-        pad_width = [(0, 0), (0, new_size - len(mfcc_features[1]))]
-        mfcc_features = np.pad(mfcc_features, pad_width, mode='constant', constant_values=0)
-    else:
-        mfcc_features = mfcc_features[:, :new_size]
-    return mfcc_features
+
+    # wav 파일에서 추출한 특징 저장
+    features = []
+
+    # [ mfcc ] 음성 데이터를 특징 벡터로 변환해주는 알고리즘
+    # shape : (20, x)
+    mfcc = librosa.feature.mfcc( y = y, sr = sr)
+    mfcc = resize(mfcc, new_size)
+    features.extend(mfcc)
+
+    # [ chroma_stft ] 옥타브는 무시하고, 12개의 음계에 대한 분포 나타냄
+    # shape : (12, x) 
+    chroma_stft = librosa.feature.chroma_stft( y, sr =sr ) 
+    chroma_stft = resize(chroma_stft, new_size)
+    features.extend(chroma_stft)
+
+    # [ zero_crossing_rate ]
+    # shape : (1, x)
+    zero_crossing_rate = librosa.feature.zero_crossing_rate( y, pad=False )
+    zero_crossing_rate = resize(zero_crossing_rate, new_size)
+    features.extend(zero_crossing_rate)
+
+    # [ spectral_centroid ]
+    # shape : (1, x)
+    spectral_centroid = librosa.feature.spectral_centroid( y, sr = sr )
+    spectral_centroid = resize(spectral_centroid, new_size)
+    features.extend(spectral_centroid)
+
+    # [ spectral_rolloff ] 
+    # shape : (1, x)
+    spectral_rolloff = librosa.feature.spectral_rolloff( y, sr = sr )
+    spectral_rolloff = resize(spectral_rolloff, new_size)
+    features.extend(spectral_rolloff)
+
+    # [ rms ] 오디오 평균 음량 측정
+    # shape : (1, x)
+    rms = librosa.feature.rms(y)
+    rms = resize(rms, new_size)
+    features.extend(rms)
+
+    features = np.array(features)
+    print(file_path, features.shape)
+    return features
 
 # wav 파일을 LSTM 모델에 적용하기 위해 데이터 전처리
-def preprocess_mfccs(mfccs,min_len = 3000,feature_size = 20):
-    new_mfccs = []
+def preprocess_features(features,min_len = 3000,feature_size = 20):
+    new_features = []
 
-    for mfcc in mfccs:
-        if mfcc.shape[1] >= min_len:
-            new_mfccs.append(mfcc[:, :min_len].T)
+    for feature in features:
+        if feature.shape[1] >= min_len:
+            new_features.append(feature[:, :min_len].T)
         else:
-            temp_mfcc = np.zeros((min_len, feature_size))
-            temp_mfcc[:mfcc.shape[1], :] = mfcc.T[:, :min_len]
-            new_mfccs.append(temp_mfcc)
+            temp_feature = np.zeros((min_len, feature_size))
+            temp_feature[:feature.shape[1], :] = feature.T[:, :min_len]
+            new_features.append(temp_feature)
 
-    new_mfccs_arr = np.array(new_mfccs)
-    return new_mfccs_arr
+    new_features_arr = np.array(new_features)
+    return new_features_arr
 
 
 def createDATA(PATH,SR):
     labels = []
-    mfccs = []
+    features = []
     global encoder
     for root, dirs, files in os.walk(PATH):
         for file in files:
@@ -41,19 +92,8 @@ def createDATA(PATH,SR):
                 file_path = os.path.join(root, file)
                 label = os.path.basename(root)
 
-                mfcc = load_wav_file(file_path,SR)
+                feature = load_wav_file(file_path,SR)
                 labels.append(label)
-                mfccs.append(mfcc)
-    mfccs = preprocess_mfccs(mfccs)
-    return mfccs, labels
-
-def preprocessing_uploadfile(file, sr=16000):
-    new_size = 3000
-    y, _ = librosa.load(file, sr=sr)
-    mfcc_features = librosa.feature.mfcc( y = y, sr = sr)
-    if len(mfcc_features[1]) < new_size:
-        pad_width = [(0, 0), (0, new_size - len(mfcc_features[1]))]
-        mfcc_features = np.pad(mfcc_features, pad_width, mode='constant', constant_values=0)
-    else:
-        mfcc_features = mfcc_features[:, :new_size]
-    return mfcc_features
+                features.append(feature)
+    features = preprocess_features(features)
+    return features, labels
