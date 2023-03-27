cdf8a45591ad8be9f2759c75346f780b7bf377af
박정희 <wjdgmll77@naver.com>
2023-03-27 10:01:01 +0900

    [AI : Feat - 모델 수정]

--- AI/module/lstm.py
+++ AI/module/lstm.py
@@ -1,12 +1,26 @@
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
+from keras.layers import Dense, LSTM, Dropout, BatchNormalization
 from keras.callbacks import ModelCheckpoint
 
 # LSTM 모델 생성
 def create_lstm_model(default=3, sr=16000, feature_size = 20):
     model = Sequential()
-    model.add(LSTM(128, input_shape=(None, feature_size)))
+    model.add(LSTM(512, input_shape=(None, feature_size), return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))      
+
+    model.add(LSTM(256, return_sequences=True))
     model.add(Dropout(0.2))
+
+    model.add(LSTM(256, return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.1))
+    
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.2))
+    
+    model.add(LSTM(64))
+    model.add(Dense(64,activation='relu'))
     model.add(Dense(default, activation='softmax'))
     return model
 
