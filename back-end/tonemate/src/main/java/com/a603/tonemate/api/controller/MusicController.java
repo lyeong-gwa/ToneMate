d059843051523be9655cab080c2d4dbc4730d5fb
RyuJeongmin <jmyoo88@naver.com>
2023-03-28 10:13:49 +0900

    [BE: Feat - 분석 결과 조회 API 구현 중]

--- back-end/tonemate/src/main/java/com/a603/tonemate/api/controller/MusicController.java
+++ back-end/tonemate/src/main/java/com/a603/tonemate/api/controller/MusicController.java
@@ -10,12 +10,11 @@ import com.a603.tonemate.security.auth.JwtTokenProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 
 @RestController
@@ -36,7 +35,7 @@ public class MusicController {
         return new ResponseEntity<String>("Alive", HttpStatus.OK);
     }
 
-    
+
     @ApiOperation(value = "음색 분석", notes = "음색 검사를 위한 녹음 wav파일을 분석 및 저장")
     @GetMapping("/timbre")
     public ResponseEntity<?> analysisTimbre() {//@CookieValue(value = JwtProperties.ACCESS_TOKEN) String token
@@ -56,10 +55,12 @@ public class MusicController {
         return new ResponseEntity<>("음색 분석", HttpStatus.OK);
     }
 
-    
+
     @ApiOperation(value = "음역대 분석", notes = "음색 검사를 위한 녹음 wav파일을 분석 및 저장")
     @GetMapping("/pitch")
     public ResponseEntity<?> analysisPitch() {//@CookieValue(value = JwtProperties.ACCESS_TOKEN) String token
+        // Post 요청으로 변경 필요, wav 파일 받아와야 함
+
         /*
          * 1. wav파일을 flask에 전달한다. -> 음역대를 계산한다. -> [최저음, 최고음]
          * 2. 잘부를 수 있는 노래, 힘들게 부를 수 있는 노래, 부를 수 없는 노래 3등분을 해서 그룹3개를 생성한다. (기준 설정해야한다.)
@@ -76,8 +77,8 @@ public class MusicController {
     @ApiOperation(value = "검사 결과 목록", notes = "사용자가 진행했던 분석결과를 리스트로 제공")
     @GetMapping("/result")
     public ResponseEntity<?> resultList() {//@CookieValue(value = JwtProperties.ACCESS_TOKEN) String token
-    	/*
-    	 * 음역대 검사, 음색검사 테이블에서 사용자에 해당하는 데이터만 추출하고 
+        /*
+         * 음역대 검사, 음색검사 테이블에서 사용자에 해당하는 데이터만 추출하고
          * */
 
         musicService.getResultList(1L);
@@ -86,16 +87,32 @@ public class MusicController {
         return new ResponseEntity<>(musicService.getResultList(1L), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "검사 결과 조회", notes = "사용자가 선택한 검사 결과의 상세 정보를 조회한다.")
+    @GetMapping("/result/{type}/{id}")
+    public ResponseEntity<?> selectOneResult(@PathVariable("type") String type, @PathVariable("id") Long id) {//@CookieValue(value = JwtProperties.ACCESS_TOKEN) String token
+        /*
+         * type에 따라서 음역대인지 음색인지 구분한다. id는 특정 테이블에 속한 결과 데이터의 id를 지정하고 해당 데이터를 조회한다..
+         * */
+
+        if (type.equals("timbre")) {
+            return new ResponseEntity<>(musicService.selectOneTimbreAnalysis(id), HttpStatus.OK);
+        } else if (type.equals("pitch")) {
+            return new ResponseEntity<>(musicService.selectOnePitchAnalysis(id), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("검사 결과 조회", HttpStatus.OK);
+    }
+
     @ApiOperation(value = "검사 결과 삭제", notes = "사용자가 선택한 검사 결과를 삭제한다.")
     @DeleteMapping("/result/{type}/{id}")
-    public ResponseEntity<?> deleteResult(@PathVariable("type") String type,@PathVariable("id") Long id) {//@CookieValue(value = JwtProperties.ACCESS_TOKEN) String token
-    	/*
-    	 * type에 따라서 음역대인지 음색인지 구분한다. id는 특정 테이블에 속한 결과 데이터의 id를 지정하고 해당 데이터를 삭제한다. 
+    public ResponseEntity<?> deleteResult(@PathVariable("type") String type, @PathVariable("id") Long id) {//@CookieValue(value = JwtProperties.ACCESS_TOKEN) String token
+        /*
+         * type에 따라서 음역대인지 음색인지 구분한다. id는 특정 테이블에 속한 결과 데이터의 id를 지정하고 해당 데이터를 삭제한다.
          * */
 
         musicService.deleteResult(type, id);
-    	
+
         return new ResponseEntity<>("검사 결과 삭제", HttpStatus.OK);
     }
-    
+
 }
