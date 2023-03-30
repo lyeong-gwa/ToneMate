c0027cc63d8ec7cb101ec71111a5a38b3eae01e9
박수민 <1379psm@naver.com>
2023-03-30 13:23:51 +0900

    [BE] Fix setCookie 설정 변경

--- back-end/tonemate/src/main/java/com/a603/tonemate/security/auth/TokenInfo.java
+++ back-end/tonemate/src/main/java/com/a603/tonemate/security/auth/TokenInfo.java
@@ -19,7 +19,7 @@ public class TokenInfo {
     public ResponseCookie generateAccessToken() {
         return ResponseCookie
                 .from(JwtProperties.ACCESS_TOKEN, this.accessToken)
-                .domain("localhost")
+//                .domain("localhost")
                 .path("/")
                 .maxAge(JwtProperties.ACCESS_TOKEN_TIME)
                 .httpOnly(true)
@@ -31,7 +31,7 @@ public class TokenInfo {
     public ResponseCookie generateRefreshToken() {
         return ResponseCookie
                 .from(JwtProperties.REFRESH_TOKEN, this.refreshToken)
-                .domain("localhost")
+//                .domain("localhost")
                 .path("/")
                 .maxAge(JwtProperties.REFRESH_TOKEN_TIME)
                 .httpOnly(true)
