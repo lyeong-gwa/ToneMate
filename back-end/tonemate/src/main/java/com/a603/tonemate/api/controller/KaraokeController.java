e6c83505179d45848942ee2c2722a31f2b2f5ad5
박수민 <1379psm@naver.com>
2023-04-04 16:20:52 +0900

    [BE] Fix 노래 검색 수정

--- back-end/tonemate/src/main/java/com/a603/tonemate/api/controller/KaraokeController.java
+++ back-end/tonemate/src/main/java/com/a603/tonemate/api/controller/KaraokeController.java
@@ -5,6 +5,7 @@ import com.a603.tonemate.dto.request.SearchSongReq;
 import com.a603.tonemate.dto.response.KaraokeResp;
 import com.a603.tonemate.security.auth.JwtProperties;
 import com.a603.tonemate.security.auth.JwtTokenProvider;
+import com.a603.tonemate.util.annotation.QueryStringArgResolver;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,7 +45,7 @@ public class KaraokeController {
 
     @ApiOperation(value = "노래 검색", notes = "tj 번호 출력")
     @GetMapping("/search")
-    public ResponseEntity<KaraokeResp> searchSong(@RequestBody(required = false) SearchSongReq searchSongReq,
+    public ResponseEntity<KaraokeResp> searchSong(@QueryStringArgResolver SearchSongReq searchSongReq,
                                                   @PageableDefault(size = 10) Pageable pageable,
                                                   @CookieValue(name = JwtProperties.ACCESS_TOKEN) String accessToken) {
         Long userId = jwtTokenProvider.getId(accessToken);
