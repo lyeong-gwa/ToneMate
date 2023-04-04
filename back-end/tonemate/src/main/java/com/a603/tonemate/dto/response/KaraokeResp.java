862cb0b270a911df1aad7a0c371cd6e7bd63f908
박수민 <1379psm@naver.com>
2023-04-04 15:41:11 +0900

    [BE] Bug 스웨거 경고 에러 해결

--- back-end/tonemate/src/main/java/com/a603/tonemate/dto/response/KaraokeResp.java
+++ back-end/tonemate/src/main/java/com/a603/tonemate/dto/response/KaraokeResp.java
@@ -14,12 +14,12 @@ public class KaraokeResp {
     @ApiModelProperty(value = "노래 정보들")
     private List<? extends KaraokeCommonDto> songs;
 
-    @ApiModelProperty(value = "한 페이지에서 나타낼수 있는 노래의 수")
+    @ApiModelProperty(value = "한 페이지에서 나타낼수 있는 노래의 수", example = "1")
     private Integer pageSize;
-    @ApiModelProperty(value = "전체 페이지 번호")
+    @ApiModelProperty(value = "전체 페이지 번호", example = "1")
     private Integer totalPageNumber;
 
-    @ApiModelProperty(value = "찾은 노래 개수")
+    @ApiModelProperty(value = "찾은 노래 개수", example = "1")
     private Long totalCount;
 
 }
--- back-end/tonemate/src/main/java/com/a603/tonemate/dto/response/ResultResp.java
+++ back-end/tonemate/src/main/java/com/a603/tonemate/dto/response/ResultResp.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @ApiModel(value = "음색/음역대 분석 검사 결과 리스트", description = "음색/음역대 검사 결과 목록의 정보들이 담김(검사 id, 검사 유형, 검사 일시)")
-public class ResultResp implements Comparable<ResultResp>{
+public class ResultResp implements Comparable<ResultResp> {
 
     @ApiModelProperty(value = "검사 결과 id")
     private Long resultId;
