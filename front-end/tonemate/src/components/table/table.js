458d43590736844c42f4f588a58aafacf6b00312
Joonha Yun <neverdcloud@gmail.com>
2023-04-07 07:20:13 +0900

    [ FE : WIP - modify table ]

--- front-end/tonemate/src/components/table/table.js
+++ front-end/tonemate/src/components/table/table.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { HeartIcon as HeartSolidIcon } from '@heroicons/react/24/solid';
+import {
+  HeartIcon as HeartSolidIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from '@heroicons/react/24/solid';
 import { HeartIcon as HeartOutlineIcon } from '@heroicons/react/24/outline';
 import { getSongs } from '@/features/karaoke';
 
@@ -17,18 +21,6 @@ function Table() {
     console.log('clickHeart');
   }
 
-  // function prevPages() {
-  //   console.log('prevPage');
-  // }
-
-  // function nextPages() {
-  //   console.log('nextPage');
-  // }
-
-  // function clickPage(page) {
-  //   console.log('clickPage' + page);
-  // }
-
   return (
     <>
       <div>
@@ -38,6 +30,7 @@ function Table() {
           <div>Error: {error.message}</div>
         ) : (
           <div className="flex h-full w-full flex-col items-center justify-start">
+            {/* Table Head : 테이블 태그로 작성시에 사이즈 조절에 문제가 생겨서 div태그로 커스터마이징 */}
             <div className="fade-in-custom-15s mb-3 flex h-10 w-full flex-row justify-between border-y border-yellow-400">
               <div className="mx-2 flex w-4/12 flex-row items-center bg-black">
                 <p className="font-nanum text-sm text-white lg:text-lg">
@@ -54,23 +47,27 @@ function Table() {
                 <p className="font-nanum text-sm text-white lg:text-lg">담기</p>
               </div>
             </div>
-            {data?.songs?.map((song) => (
-              <div className="fade-in-custom-15s mb-2 flex w-full flex-col" key={song.tjNum}>
-                <div className="mb-1 flex h-14 w-full flex-row justify-between rounded-full bg-white opacity-40">
+            {/* Table Body : 테이블 태그로 작성시에 사이즈 조절에 문제가 생겨 div태그로 커스터마이징 */}
+            <div className="fade-in-custom-15s mb-2 flex w-full flex-col">
+              {data?.songs?.map((song) => (
+                <div
+                  key={song?.tjNum}
+                  className="mb-1 flex h-14 w-full flex-row justify-between rounded-full bg-white opacity-40"
+                >
                   <div className="mx-2 flex w-4/12 flex-row items-center ">
                     <p className="font-nanum text-sm text-white lg:text-lg">
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{song.title}
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{song?.title}
                     </p>
                   </div>
                   <div className="flex w-4/12 flex-row items-center justify-center ">
-                    <p className="font-nanum text-sm text-white lg:text-lg">{song.singer}</p>
+                    <p className="font-nanum text-sm text-white lg:text-lg">{song?.singer}</p>
                   </div>
                   <div className="flex w-1/12 flex-row items-center justify-center ">
-                    <p className="font-nanum text-sm text-white lg:text-lg">{song.tjNum}</p>
+                    <p className="font-nanum text-sm text-white lg:text-lg">{song?.tjNum}</p>
                   </div>
                   <div className="mx-2 flex w-1/12 flex-row items-center justify-center ">
                     <button onClick={clickHeart}>
-                      {song.isLike ? (
+                      {song?.isLike ? (
                         <HeartSolidIcon className="h-4 w-4 text-white lg:h-5 lg:w-5" />
                       ) : (
                         <HeartOutlineIcon className="h-4 w-4 text-white lg:h-5 lg:w-5" />
@@ -78,67 +75,28 @@ function Table() {
                     </button>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         )}
-        <span>Current Page: {page}</span>
+        {isFetching ? <span> Loading...</span> : null}
+      </div>
+      <div className="fade-in-custom-20s mb-2 flex h-10 w-2/5 flex-row items-center justify-between lg:w-1/5">
+        <button onClick={() => setPage((old) => Math.max(old - 1, 1))} disabled={page === 1}>
+          <ChevronLeftIcon className="h-6 w-6 text-white" />
+        </button>
+        <span className="font-nanum text-sm text-white lg:text-lg">{page}</span>
         <button
           onClick={() => {
-            if (page !== data.totalPageNumber) {
+            if (page !== data?.totalPageNumber) {
               setPage((old) => old + 1);
             }
           }}
-          // Disable the Next Page button until we know a next page is available
-          // disabled={isPreviousData || !data?.hasMore}
+          disabled={page === data?.totalPageNumber}
         >
-          Next Page
+          <ChevronRightIcon className="h-6 w-6 text-white" />
         </button>
-        {isFetching ? <span> Loading...</span> : null}{' '}
       </div>
-      {/* <div className="flex h-full w-full flex-col items-center justify-start"> */}
-      {/* <div className="fade-in-custom-20s mb-2 flex h-10 w-2/5 flex-row items-center justify-between lg:w-1/5">
-          <button onClick={prevPages}>
-            <ChevronLeftIcon className="h-6 w-6 text-white" />
-          </button>
-          <button onClick={() => setPage((old) => Math.max(old - 1, 1))} disabled={page === }>
-           <ChevronLeftIcon className="h-6 w-6 text-white" />
-          </button>
-          <button
-            onClick={() => clickPage(basePage)}
-            className="font-nanum text-sm text-white lg:text-lg"
-          >
-            {basePage}
-          </button>
-          <button
-            onClick={() => clickPage(basePage + 1)}
-            className="font-nanum text-sm text-white lg:text-lg"
-          >
-            {basePage + 1}
-          </button>
-          <button
-            onClick={() => clickPage(basePage + 2)}
-            className="font-nanum text-sm text-white lg:text-lg"
-          >
-            {basePage + 2}
-          </button>
-          <button
-            onClick={() => clickPage(basePage + 3)}
-            className="font-nanum text-sm text-white lg:text-lg"
-          >
-            {basePage + 3}
-          </button>
-          <button
-            onClick={() => clickPage(basePage + 4)}
-            className="font-nanum text-sm text-white lg:text-lg"
-          >
-            {basePage + 4}
-          </button>
-          <button onClick={nextPages}>
-            <ChevronRightIcon className="h-6 w-6 text-white" />
-          </button>
-        </div> */}
-      {/* </div> */}
     </>
   );
 }
