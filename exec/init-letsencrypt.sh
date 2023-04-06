8513d3a70bb18c259e390d48060c16da152bcf93
wjdgmll77 <wjdgmll77@naver.com>
2023-04-06 04:24:45 +0000

    [develop: https 작업 중 ]

--- exec/init-letsencrypt.sh
+++ exec/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=j8a603.p.ssafy.io
 rsa_key_size=4096
-data_path="./certbot"
+data_path="./data/certbot"
 email="wjdgmll77@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
